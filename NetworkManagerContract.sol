fd23e883bed019258e3156b743b0a76491b97850
golashr <rahul.g@ledgerium.net>
2019-03-13 13:22:35 +1100

    updated NetworkManagerContract.sol contract

--- NetworkManagerContract.sol
+++ NetworkManagerContract.sol
@@ -3,67 +3,70 @@ contract NetworkManagerContract {
 
     uint nodeCounter;
   
-    struct NodeDetails {
-  
+    struct NodeDetails {  
         string nodeName;
         string role; 
         string publickey;
         string enode;
-	string ip;
+        string ip;
+        string id;
     }
 
     mapping (string => NodeDetails)nodes;
     string[] enodeList;
     
-    event print(string nodeName, string role,string publickey, string enode, string ip);
+    event print(string nodeName, string role,string publickey, string enode, string ip, string id);
 
-    function registerNode(string memory n, string memory r, string memory p,string memory e, string memory ip) public {
+    function registerNode(string memory n, string memory r, string memory p,string memory e, string memory ip, string memory id) public {
 
         nodes[e].publickey = p;
         nodes[e].nodeName = n;
         nodes[e].role = r;
-	nodes[e].ip = ip;
+        nodes[e].ip = ip;
+        nodes[e].id = id;
         enodeList.push(e);
-        //emit print(n,r,p,e,ip);
+        emit print(n, r, p, e, ip, id);
     
     }
    
-    function getNodeDetails(uint _index) view public returns (string memory n, string memory r,string memory p,string memory ip,string memory e,uint i) {
+    function getNodeDetails(uint16 _index) view public returns (string memory n, string memory r, string memory p, string memory ip, string memory e, string memory id, uint i) {
         NodeDetails memory nodeInfo = nodes[enodeList[_index]];
         return (
-                nodeInfo.nodeName,
-                nodeInfo.role,
-                nodeInfo.publickey,
-		nodeInfo.ip,
-                enodeList[_index],
-                _index
+            nodeInfo.nodeName,
+            nodeInfo.role,
+            nodeInfo.publickey,
+            nodeInfo.ip,
+            enodeList[_index],
+            nodeInfo.id,
+            _index
         );
     }
 
-    function getNodesCounter() public view  returns (uint) {
+    function getNodesCounter() public view returns (uint) {
         return enodeList.length;
     }
 
-    function updateNode(string memory n, string memory r, string memory p, string memory e, string memory ip) public {
+    function updateNode(string memory n, string memory r, string memory p, string memory e, string memory ip, string memory id) public {
 
         nodes[e].publickey = p;
         nodes[e].nodeName = n;
         nodes[e].role = r;
-	nodes[e].ip = ip;
-        //emit print(n,r,p,e,ip);
+        nodes[e].ip = ip;
+        nodes[e].id = id;
+        emit print(n, r, p, e, ip, id);
     }
 
-    function getNodeList(uint i)  public  view   returns (string memory n, string memory r,string memory p,string memory ip,string memory e) {
+    function getNodeList(uint16 i)  public view returns (string memory n, string memory r, string memory p, string memory ip, string memory e, string memory id) {
         
         NodeDetails memory nodeInfo = nodes[enodeList[i]];
         return (
             nodeInfo.nodeName,
             nodeInfo.role,
             nodeInfo.publickey,
-	    nodeInfo.ip,
-            enodeList[i]
+	        nodeInfo.ip,
+            enodeList[i],
+            nodeInfo.id
         );
-        
     }
 
 }
